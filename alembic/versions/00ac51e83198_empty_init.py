b960dba0238afc36636fc4e3f1fa10f389885b66
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-08-05 09:26:34 -0400

    fix: package updates; initialized alembic  	new file:   alembic.ini  	new file:   alembic/README  	new file:   alembic/env.py  	new file:   alembic/script.py.mako  	new file:   alembic/versions/00ac51e83198_empty_init.py  	new file:   config.py  	modified:   pyproject.toml

---
+++ alembic/versions/00ac51e83198_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: 00ac51e83198
+Revises: 
+Create Date: 2023-08-05 08:52:21.147008
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00ac51e83198'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ config.py
@@ -0,0 +1,4 @@
+# Specify where the affiliations database lives
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg://user:pwd@localhost:5432/affiliations'
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
