073ab272816fca3b190de162ea15eb76e071e7ab
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2024-01-07 09:38:59 -0500

    modified:   alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -56,7 +56,7 @@ def get_app_config(key):
     if opath not in sys.path:
         sys.path.insert(0, opath)
 
-    from adsmp.tasks import app
+    from adsaffildb.tasks import app
 
     print("Getting actual config for", key, app.conf.get(key))
     return app.conf.get(key)
