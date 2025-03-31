b2d1da070c25cb93654b669a0d03f8293a4986a7
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2025-03-31 19:24:26 -0400

    testing  	modified:   run.py

--- run.py
+++ run.py
@@ -67,7 +67,7 @@ def main():
         dataParentChild = utils.read_flat_files(infile,
                                                 with_header,
                                                 delimiter)
-        uniqueParentChild = merge_parents(dataParentChild)
+        uniqueParentChild = utils.merge_parents(dataParentChild)
         write_to_database(affil_inst, dataParentChild)
         
 
