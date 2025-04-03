8c9b3be1a1b238db8c2bc85a6367f6336dd7bcc6
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2025-04-03 13:34:16 -0400

    testing  	modified:   affildb/database.py

--- affildb/database.py
+++ affildb/database.py
@@ -69,6 +69,6 @@ def fetch_data_table(app, table):
 def query_distinct_norm(app, table):
     with app.session_scope() as session:
         try:
-            return session.query(table).join().distinct(table.norm_string).all()
+            return session.query(table).distinct(table.norm_string).all()
         except Exception as err:
             raise DBQueryException("Unable to query %s for distinct normalized strings: %s" % (str(table), err))
