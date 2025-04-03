66f4edd6776420a160c6cacfcb1f8b678bf5834b
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2025-04-03 10:35:31 -0400

    testing  	modified:   alembic/versions/bb9547f406c7_affildb_tables.py

--- alembic/versions/bb9547f406c7_affildb_tables.py
+++ alembic/versions/bb9547f406c7_affildb_tables.py
@@ -58,8 +58,8 @@ def upgrade() -> None:
         sa.Column("norm_string", sa.String(), nullable=False),
         sa.Column("notes", sa.String(), nullable=False, default=""),
         sa.Column("created", UTCDateTime, nullable=True, default=get_date),
-        sa.PrimaryKeyConstraint("curation_key", "data_key", name="dpairs"),
-        sa.PrimaryKeyConstraint("norm_string", "affil_id", name="npairs"),
+        sa.PrimaryKeyConstraint("curation_key", "data_key", "affil_id", "norm_string", name="dpairs"),
+        #sa.PrimaryKeyConstraint("norm_string", "affil_id", name="npairs"),
     )
 
 
