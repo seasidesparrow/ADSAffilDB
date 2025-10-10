0e854efbfb2ed55855d2331fd0462eccb7910f8e
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2025-10-10 19:37:40 -0400

    add gin index  	modified:   affildb/models.py

--- affildb/models.py
+++ affildb/models.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from adsmutils import get_date, UTCDateTime
 
-from sqlalchemy import Column, Integer, String, Text, Boolean
+from sqlalchemy import Column, Integer, String, Text, Boolean, Index
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -54,6 +54,7 @@ class AffilData(Base):
     """
 
     __tablename__ = "affil_data"
+    __table_args__ = (Index('norm_index', norm_string, postgresql_using="gin"),)
 
     data_key = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     affil_id = Column(String(6), primary_key=True, unique=False, nullable=False)
